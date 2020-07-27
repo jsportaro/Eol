17d692de53a8b19cf5c4827fbd845c5dc22ae092
Joseph Portaro <jsportaro@outlook.com>
2020-07-27 13:57:32 -0400

    First program

--- Eol.Runner/Program.cs
+++ Eol.Runner/Program.cs
@@ -8,17 +8,23 @@ namespace Eol.Runner
         {
             var eater = new Eater(new Gpio());
 
+            Console.WriteLine("Programming the Eater");
+            eater.SetByte(0b1110_0000, 0b1000);
+            eater.SetByte(0b1111_0000, 0b0001);
+            Console.WriteLine("Done");
             while (true)
             {
-                eater.SetByte(0b0000_0000, 0b0000);
-                eater.SetByte(0b0000_0001, 0b0001);
-                eater.SetByte(0b0000_0010, 0b0010);
-                eater.SetByte(0b0000_0100, 0b0011);
-                eater.SetByte(0b0000_1000, 0b0100);
-                eater.SetByte(0b0001_0000, 0b0101);
-                eater.SetByte(0b0010_0000, 0b0110);
-                eater.SetByte(0b0100_0000, 0b1000);
-                eater.SetByte(0b1000_0000, 0b1001);
+                //eater.SetByte(0b0000_0000, 0b0000);
+                //eater.SetByte(0b0000_0001, 0b0001);
+                //eater.SetByte(0b0000_0010, 0b0010);
+                //eater.SetByte(0b0000_0100, 0b0011);
+                //eater.SetByte(0b0000_1000, 0b0100);
+                //eater.SetByte(0b0001_0000, 0b0101);
+                //eater.SetByte(0b0010_0000, 0b0110);
+                //eater.SetByte(0b0100_0000, 0b1000);
+                //eater.SetByte(0b1000_0000, 0b1001);
+
+
             }
         }
     }
